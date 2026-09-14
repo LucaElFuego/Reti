60559047d7f597e65c552978711da7a387ad0c4e
LucaElFuego <l.picco.4108@vallauri.edu>
2026-09-14 11:55:36 +0200

    Finito Es 1

--- Puntatori/Es_1_sizeof/Es_1_sizeof.c
+++ Puntatori/Es_1_sizeof/Es_1_sizeof.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Ciao Mondo!\n");
+//Comando per compilare e aggiornare il .exe senza crearne uno nuovo
+//gcc Es_1_sizeof.c -o Es_1_sizeof.exe
+
+int main(void) 
+{
+    int i; //0x005 primo indirizzo
+    i = 10;
+    char c = 'c'; //0x00A
+
+    int *pi; // 0x01B
+
+    printf("La variabile i occupa %d byte\n", sizeof(i));
+    printf("Il tipo char occupa %d byte\n", sizeof(char));
+    printf("La variabile pi* occupa %d byte\n", sizeof(pi));
+
+    printf("L'indirizzo di i e' %p e contiene %d\n",&i, i);
+    printf("L'indirizzo di c e' %p e contiene %c\n",&c, c);
+    printf("L'indirizzo di pi e' %p e contiene %p\n",&pi, pi);
     return 0;
 }
